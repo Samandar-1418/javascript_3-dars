34dd1b9c335444776039f815348c81a7c82bd9fa
Samandar1418 <nabiyevs967@gmail.com>
2023-10-11 21:33:12 +0500

    start-finish

---
+++ index.js
@@ -0,0 +1,141 @@
+// 1 masala 
+// let n = prompt ("son kiriting");
+// for (let i=1; i<=n; i ++ ){
+//     console.log(i);
+// }
+
+// 2 masala 
+// let n = prompt (" son kiriting");
+// for (let i = n; i >= 1; i--){
+//     console.log(i);
+// }
+// 3 masala 
+// let n = prompt ("son kiriting");
+// let sum = 0;
+// for (let i = 1; i <= n; i++){
+//     sum += i;
+// }
+// console.log(sum);
+// 4 masala 
+// let n= prompt ("son kiriting");
+// let sum = 0;
+// for (let i = 1 ; i <= n ; i++) {
+//   
+//     if (i % 2 == 0 ){
+        
+//         console.log(i);
+//       sum += i;
+//     }
+// }
+// console.log (sum);
+// 5 masala 
+// let n= prompt ("son kiriting");
+// let sum = 0;
+// for (let i = 1 ; i <= n ; i++) {
+ 
+//     if (i % 2 == 1 ){  
+//         console.log(i);
+//         sum += i;
+//     }
+// }
+// console.log (sum);
+// 6 masala 
+// let a = prompt ("son kiriting");
+// for (let i =   )
+// 7 masala 
+// 8 masala 
+// for1
+// let k = prompt('kni kirit');
+// let N = prompt('ni kirit'); 
+// for (let i = 0; i < N; i++) {
+//     console.log(k);
+// }
+_
+// for2  
+// let a = prompt("son kiriting:");
+// let b = prompt("ikkinchi sonni kiriting:");
+// sum = 0;
+// for (let i = a; i <= b; i++) {
+//     if(a!=i&&b!=i) {
+//         sum++
+//         console.log(i);
+//     }
+
+// }
+// console.log('soni ' ,sum);
+
+// for3
+// let a = prompt("a ni kiriting");
+// let b = prompt("b ni kiriting");
+
+//   for (let i = b; i >= a; i--) {
+//     if (a != i && b != i) {
+//       console.log(i);
+//     } else if (a == i && b == i) {
+//       console.log(i);
+//     }
+//   }
+
+// for4
+// let a = prompt("narxni kiriting: ")
+// let sum = 0;
+// for (let i = 1; i <=10; i++){
+//     console.log(i,"kg",i*a, "sum");
+// }
+
+// for5
+// let a = prompt("narxni kiriting: ")
+// let sum = 0;
+// for (let i = 0.1; i <= 1; i += 0.1){
+//     console.log(i,"kg",i*a, "sum");
+// }
+
+// for6
+// let a = prompt("narh kiriting ")
+// let sum = 0;
+// for (let i = 1.2; i <= 2; i += 0.2){
+//     console.log(i,"kg",i*a, "sum");
+// }
+
+// for7
+// let a = prompt("birinchi sonni kiriting:");
+// let b = prompt(" sonni kiriting:");
+// a = a*1;
+// b = b*1;
+// sum = 0;
+// for (let i = a; i <= b; i++) {
+//     if(a >= 0 && b >= 0) {
+//         sum = sum + i
+//     } else {
+//         console.log("Bu yerda manfiy son bor!");
+//     }
+
+// }
+// console.log("yig'indi " ,sum);
+
+// for8
+// let a = prompt("sonni kiriting: ");
+// let b = prompt("ikkinchi sonni kiriting")
+
+// sum = 1;
+// for (let i = a; i <= b; i++) {
+//     sum = sum * i
+// }
+// console.log(sum);
+
+// for9
+
+// let a = prompt("sonni kiriting: ");
+// let b = prompt("ikkinchi sonni kiriting")
+
+// kv = 1;
+// res= 0;
+
+// for (let i = a; i <= b; i++) {
+//     kv = i ** 2;
+//     res += kv;
+//     console.log(kv);
+// }
+// console.log(sum);
+
+
